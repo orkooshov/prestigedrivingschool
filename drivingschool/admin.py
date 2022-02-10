ab83b852698c917de988d147390f73064321f502
orkooshov <orkooshov@gmail.com>
2022-02-10 21:25:29 +0300

    Duplicated code was removed again

--- drivingschool/admin.py
+++ drivingschool/admin.py
@@ -8,21 +8,13 @@ class DefaultAdmin(admin.ModelAdmin):
 
 
 class TutorAdmin(admin.ModelAdmin):
-    # exclude = ('id', )
     list_display = ['id', '__str__', 'cabinet']
 
 
-admin.site.register(m.Cabinet, DefaultAdmin)
+admin.site.register(
+    (
+        m.Cabinet, m.Group, m.TopicTheory, m.TopicPractice, m.LessonTheory, 
+        m.LessonPractice, m.ScheduleTheory, m.SchedulePractice, m.Car,
+        m.CarBrand, m.CarModel, m.Instructor, m.Student
+    ), DefaultAdmin)
 admin.site.register(m.Tutor, TutorAdmin)
-admin.site.register(m.Group, DefaultAdmin)
-admin.site.register(m.TopicTheory, DefaultAdmin)
-admin.site.register(m.TopicPractice, DefaultAdmin)
-admin.site.register(m.LessonTheory, DefaultAdmin)
-admin.site.register(m.ScheduleTheory, DefaultAdmin)
-admin.site.register(m.CarBrand, DefaultAdmin)
-admin.site.register(m.CarModel, DefaultAdmin)
-admin.site.register(m.Car, DefaultAdmin)
-admin.site.register(m.Instructor, DefaultAdmin)
-admin.site.register(m.Student, DefaultAdmin)
-admin.site.register(m.SchedulePractice, DefaultAdmin)
-admin.site.register(m.LessonPractice, DefaultAdmin)
