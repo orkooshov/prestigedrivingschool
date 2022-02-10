30c0e99c87d1211ed76df817efa4699b8d45b206
orkooshov <orkooshov@gmail.com>
2022-02-10 21:21:41 +0300

    Duplicated code was removed

--- drivingschool/admin.py
+++ drivingschool/admin.py
@@ -3,77 +3,26 @@ from . import models as m
 from django.utils.translation import gettext as _
 
 
-class CabinetAdmin(admin.ModelAdmin):
+class DefaultAdmin(admin.ModelAdmin):
     exclude = ('id', )
 
 
 class TutorAdmin(admin.ModelAdmin):
-    exclude = ('id', )
+    # exclude = ('id', )
+    list_display = ['id', '__str__', 'cabinet']
 
 
-class GroupAdmin(admin.ModelAdmin):
-    exclude = ('id', )
-
-
-class TopicTheoryAdmin(admin.ModelAdmin):
-    exclude = ('id', )
-
-
-class TopicPracticeAdmin(admin.ModelAdmin):
-    exclude = ('id', )
-
-
-class LessonTheoryAdmin(admin.ModelAdmin):
-    exclude = ('id', )
-
-
-class ScheduleTheoryAdmin(admin.ModelAdmin):
-    exclude = ('id', )
-
-
-class CarBrandAdmin(admin.ModelAdmin):
-    exclude = ('id', )
-
-
-class CarModelAdmin(admin.ModelAdmin):
-    exclude = ('id', )
-
-
-class CarTransmissionAdmin(admin.ModelAdmin):
-    exclude = ('id', )
-
-
-class CarAdmin(admin.ModelAdmin):
-    exclude = ('id', )
-
-
-class InstructorAdmin(admin.ModelAdmin):
-    exclude = ('id', )
-
-
-class StudentAdmin(admin.ModelAdmin):
-    exclude = ('id', )
-
-
-class SchedulePracticeAdmin(admin.ModelAdmin):
-    exclude = ('id', )
-
-
-class LessonPracticeAdmin(admin.ModelAdmin):
-    exclude = ('id', )
-
-
-admin.site.register(m.Cabinet, CabinetAdmin)
+admin.site.register(m.Cabinet, DefaultAdmin)
 admin.site.register(m.Tutor, TutorAdmin)
-admin.site.register(m.Group, GroupAdmin)
-admin.site.register(m.TopicTheory, TopicTheoryAdmin)
-admin.site.register(m.TopicPractice, TopicPracticeAdmin)
-admin.site.register(m.LessonTheory, LessonTheoryAdmin)
-admin.site.register(m.ScheduleTheory, ScheduleTheoryAdmin)
-admin.site.register(m.CarBrand, CarBrandAdmin)
-admin.site.register(m.CarModel, CarModelAdmin)
-admin.site.register(m.Car, CarAdmin)
-admin.site.register(m.Instructor, InstructorAdmin)
-admin.site.register(m.Student, StudentAdmin)
-admin.site.register(m.SchedulePractice, SchedulePracticeAdmin)
-admin.site.register(m.LessonPractice, LessonPracticeAdmin)
+admin.site.register(m.Group, DefaultAdmin)
+admin.site.register(m.TopicTheory, DefaultAdmin)
+admin.site.register(m.TopicPractice, DefaultAdmin)
+admin.site.register(m.LessonTheory, DefaultAdmin)
+admin.site.register(m.ScheduleTheory, DefaultAdmin)
+admin.site.register(m.CarBrand, DefaultAdmin)
+admin.site.register(m.CarModel, DefaultAdmin)
+admin.site.register(m.Car, DefaultAdmin)
+admin.site.register(m.Instructor, DefaultAdmin)
+admin.site.register(m.Student, DefaultAdmin)
+admin.site.register(m.SchedulePractice, DefaultAdmin)
+admin.site.register(m.LessonPractice, DefaultAdmin)
