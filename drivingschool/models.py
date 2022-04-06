6a8be4a1d1275b259bd7068405c1eedb30acf4fe
orkooshov <orkooshov@gmail.com>
2022-04-07 02:13:35 +0300

    remove unused imports

--- drivingschool/models.py
+++ drivingschool/models.py
@@ -8,7 +8,6 @@ from django.utils.translation import gettext_lazy as _
 from django.urls import reverse
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.core.validators import validate_email
 
 
 class Gender(models.TextChoices):
