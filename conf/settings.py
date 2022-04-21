f4bf175f2f9c1107e7d2c474e38aa9b2ad35c946
orkooshov <orkooshov@gmail.com>
2022-04-21 10:26:22 +0300

    debug toolbar added

--- conf/settings.py
+++ conf/settings.py
@@ -16,7 +16,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY',
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
 
 # Application definition
 
@@ -27,7 +29,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'rest_framework',
+    'debug_toolbar',
 
     'drivingschool',
 ]
@@ -46,6 +50,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'conf.urls'
